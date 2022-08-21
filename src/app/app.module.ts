8f84a9c3652704ee92ab2e7bd63e69fb9b00c360
Dmitriy Limasov <dv.limasov@gmail.com>
2022-08-21 16:48:31 +0300

    add form controls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Step1Component } from './components/step1/step1.component';
+import { Step2Component } from './components/step2/step2.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
-    Step1Component
+    Step1Component,
+    Step2Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/step1/step1.component.ts
+++ src/app/components/step1/step1.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-step1',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Step1Component implements OnInit {
 
+  formGroup: FormGroup;
+
+  @Input() formError = '';
+
   constructor() { }
 
   ngOnInit(): void {
+    this.formGroup = new FormGroup({
+      login: new FormControl('login', [Validators.required]),
+      password: new FormControl( '12345', [Validators.required])
+    })
   }
 
 }
